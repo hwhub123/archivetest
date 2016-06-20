5ddce094e1ec9a9f09985af5cd94990574e8e99e
F839-014 M <mf839-014@70-12-244-35.pools.spcsdns.net>
2016-06-20 10:15:59 +0900

    0620_test_1

--- ArchiveTest/main.m
+++ ArchiveTest/main.m
@@ -11,7 +11,16 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSDictionary *glossary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                   @"abstract class",
+                                  @"A class defined so other classes can inherit from it",
+                                  @"adopt",
+                                  @"To implement all the methods defined in a protocol",
+                                  @"archiving",
+                                  @"Storing an object for later use", nil];
+
+        if( [glossary writeToFile:@"glossary" atomically: YES] == NO )
+            NSLog(@"Save to file failes!");
     }
     return 0;
 }
